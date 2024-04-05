abb9ead536d18411c3c2c43ce289fab89f8c07e4
ST_Dmitry <suyarkov@gmail.com>
2024-04-05 13:21:09 +0300

    А чтоб все собиралось.

---
+++ Win32/Debug/Classes.snippet_Insert.pas
@@ -0,0 +1,17 @@
+unit Classes.snippet_Insert;
+
+interface
+
+type
+  Tsnippet_Insert = class
+
+  private
+    Fsnippet: Tsnippet;
+
+  public
+    property language: string read Flanguage write Flanguage;
+  end;
+
+implementation
+
+end.
